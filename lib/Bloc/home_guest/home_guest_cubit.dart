9111fa211d4be7ee5143361040235db95c64c9bd
batoulkj <batoulkhadamaljame@gmail.com>
2023-01-13 22:19:35 +0300

    android

--- lib/Bloc/home_guest/home_guest_cubit.dart
+++ lib/Bloc/home_guest/home_guest_cubit.dart
@@ -7,7 +7,6 @@ import 'package:consulting_app/network/remote/end_point.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
 
-
 class HomeGuestCubit extends Cubit<HomeDataGuestStates> {
   HomeGuestCubit() : super(HomeGuestInitialState());
 
@@ -33,10 +32,7 @@ class HomeGuestCubit extends Cubit<HomeDataGuestStates> {
     emit(LoadingHomeDataGuestState());
 
     DioHelper.getData(
-      url:
-      id == 0
-          ? HOME:
-      'home/$id',
+      url: id == 0 ? HOME : 'home/$id',
     ).then((value) {
       homeGuestModel = HomeModel.fromJson(value.data);
       // homeModel!.data!.experts.forEach((element) {
